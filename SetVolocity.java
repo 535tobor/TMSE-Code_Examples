52762ea1e0d55c54aea32b02111ea429621930e8
535tobor <92122791+535tobor@users.noreply.github.com>
2023-12-09 15:04:39 -0500

    Update SetVolocity.java

--- SetVolocity.java
+++ SetVolocity.java
@@ -1,1 +1,48 @@
 // set volocity is a better way to move the robot instead of using set power, especially for autonomous.
+/*
+This example code was found on game manuel zero
+at https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+ */
+
+package org.firstinspires.ftc.teamcode.reference.gm0_examples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class SetVolocity extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("fl");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("bl");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("fr");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("br");
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            int speed = 5000;
+
+            frontLeftMotor.setVolocity(speed);
+            backLeftMotor.setVolocity(speed);
+            frontRightMotor.setVolocity(speed);
+            backRightMotor.setVolocity(speed);
+        }
+    }
+}
